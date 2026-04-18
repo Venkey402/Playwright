ec996db28788cede45b3894807202f665e31d6e9
venkat <venkey402@gmail.com>
2026-04-18 10:29:01 +0530

    test commit

--- Automation/tests/Browser_context_page.spec.js
+++ Automation/tests/Browser_context_page.spec.js
@@ -15,4 +15,5 @@ const page = await context.newPage();
 await page.setViewportSize({width:1920,height:1080})
 await page.goto("https://playwright.dev/docs/test-reporters#custom-reporters")
 await page.pause()
+// await page.close()
 })
